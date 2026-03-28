005be49715273b6e11f6fff1ce6fd66fe81daa9d
Vini <zmisterfire@gmail.com>
2026-03-28 02:21:58 -0300

    fix porta render

--- server.js
+++ server.js
@@ -111,6 +111,8 @@ app.delete("/agendamentos/:id", (req, res) => {
 });
 
 // ================= SERVIDOR =================
-app.listen(3000, () => {
-  console.log("🚀 Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor rodando");
 });
